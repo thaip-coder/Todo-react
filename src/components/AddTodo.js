48d4ec62ab3c874131ad9f7686369f3ac4a33610
Thai Phan <thaiphan90@yahoo.com>
2020-04-27 23:19:43 -0500

    added comments for better understanding

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -5,6 +5,7 @@ export class AddTodo extends Component {
         title: ''
     }
 
+    // Updates the state of the text box value as user types in todo item
     onChange = (e) => this.setState({ title: e.target.value });
 
     render() {
@@ -14,8 +15,10 @@ export class AddTodo extends Component {
                 type='text' 
                 name='title' 
                 style={{ flex: '10', paddiong: '5px'}}
-                placeholder='Add Todo ...' 
+                placeholder='Add Todo ...'
+                // Pulls in value of the state 
                 value={this.state.title}
+                // Changes vale of the state via onChange method
                 onChange={this.onChange}
                 />
                 <input 
