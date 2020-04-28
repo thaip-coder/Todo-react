a714ea61c0201614f6484dabc39c5166951d0234
Thai Phan <thaiphan90@yahoo.com>
2020-04-27 22:02:25 -0500

    created state objext of todos list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos'
 
-function App() {
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with girlfriend',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      },
+    ]
+  };
+
+render() {
+  console.log(this.state.todos)
   return (
     <div className="App">
-      <h1>App</h1>
+      <Todos todos={this.state.todos} />
     </div>
   );
+}
 }
 
 export default App;
